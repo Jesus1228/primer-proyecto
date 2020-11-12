98dffee9eb097a5c5a620d353e768462e7154d41
Jesús <albcienjesus@gmail.com>
2020-11-12 11:14:06 -0600

    Ahora se agregó más código

--- Prueba_uno.py
+++ Prueba_uno.py
@@ -1,3 +1,14 @@
 variable = 'Hola'
 variable2 = 'mundo'
 print(variable, variable2)
+
+class Cacatua:
+    def __init__(self, nombre, sonido):
+        self.nombre = nombre
+        self.sonido = sonido
+
+    def Patada(self):
+        print('Te pateo', self.nombre)
+
+cacatua = Cacatua('Ronie', 'gruñe')
+cacatua.Patada()
